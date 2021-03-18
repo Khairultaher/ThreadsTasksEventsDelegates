9c3b9ad05f8749652aeef9a7b8f704153ea58d38
Khairul Taher <khairultaher@outlook.com>
2021-03-18 15:55:58 +0600

    2021-03-18 1.4

--- ThreadsTasks/Program.cs
+++ ThreadsTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,10 +15,15 @@ namespace ThreadsTasks
             Console.WriteLine("Main Thread: " 
                 + Thread.CurrentThread.ManagedThreadId.ToString() + " is running");
 
+            DateTime dt1 = DateTime.Now;
             await clsSender.LongRunningProcessAsync(1);
-
             await clsSender.LongRunningProcessAsync(2);
+            DateTime dt2 = DateTime.Now;
+            TimeSpan diff = dt2 - dt1;
+            Console.WriteLine($"First block running synchronously & ended with {diff}");
 
+
+            dt1 = DateTime.Now;
             new Thread(() =>
             {
                 clsSender.LongRunningProcess(3);
@@ -28,6 +34,22 @@ namespace ThreadsTasks
             {
                clsSender.LongRunningProcess(4);
             });
+
+            dt2 = DateTime.Now;
+            diff = dt2 - dt1;
+            Console.WriteLine($"Second block running in separate thread & ended with {diff}");
+
+
+
+            dt1 = DateTime.Now;
+            clsSender.LongRunningProcessAsync(5);
+
+            await clsSender.LongRunningProcessAsync(6);
+
+            dt2 = DateTime.Now;
+            diff = dt2 - dt1;
+            Console.WriteLine($"Third block running asynchronously ended with {diff}");
+
 
             Console.WriteLine("Main Thread: "
                        + Thread.CurrentThread.ManagedThreadId.ToString() + " is end");
@@ -40,23 +62,34 @@ namespace ThreadsTasks
     {
         public void LongRunningProcess(int num)
         {
+            DateTime dt1 = DateTime.Now;
             Console.WriteLine($"Long Running Process {num} Started with Thread " 
                 + Thread.CurrentThread.ManagedThreadId.ToString());
             Thread.Sleep(5000);
             // Task.Delay(5000);
+
+            DateTime dt2 = DateTime.Now;
+            TimeSpan diff = dt2 - dt1;
             Console.WriteLine($"Long Running Process {num} Ended with " 
-                + Thread.CurrentThread.ManagedThreadId.ToString());
+                + Thread.CurrentThread.ManagedThreadId.ToString() + $" {diff}");
 
         }
 
         public async Task LongRunningProcessAsync(int num)
         {
+            DateTime dt1 = DateTime.Now;
             Console.WriteLine($"Long Running Process {num} Started with Thread "
                 + Thread.CurrentThread.ManagedThreadId.ToString());
-            //Thread.Sleep(5000);
+
             await Task.Delay(5000);
+
+            //HttpClient httpClient = new HttpClient();
+            //var data = await httpClient.GetAsync("https://stackoverflow.com");
+
+            DateTime dt2 = DateTime.Now;
+            TimeSpan diff = dt2 - dt1;
             Console.WriteLine($"Long Running Process {num} Ended with "
-                + Thread.CurrentThread.ManagedThreadId.ToString());
+                + Thread.CurrentThread.ManagedThreadId.ToString() + $" {diff}");
 
         }
     }
