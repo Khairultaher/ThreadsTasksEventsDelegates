3b0bcdfbd78539e02222ac72107da4a9b3405875
Khairul Taher <khairultaher@outlook.com>
2021-05-06 22:28:00 +0600

    2021-05-06.1

---
+++ Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelClass delClass = new DelClass();
+            delClass.LogRunningProc(CallBackMethod);
+
+            Console.ReadLine();
+        }
+
+        static void CallBackMethod(int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    public class DelClass 
+    {
+        public delegate void CallBackDel(int num);
+        public void LogRunningProc(CallBackDel callBack)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                callBack(i + 1);
+            }
+        }
+    }
+}
